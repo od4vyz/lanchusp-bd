92701537cd35fde721b1f532bcf04180a53af356
André Portela <andreplino@gmail.com>
2025-11-24 19:42:01 -0300

    CRUD Receita

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from rotas.cliente import cliente_blueprint
 from rotas.funcionario import funcionario_blueprint
 from rotas.lanchonete import lanchonete_blueprint
 from rotas.item import item_blueprint
+from rotas.receita import receita_blueprint
 
 app = Flask(__name__)
 # Permite qualquer IP acessar a database
@@ -18,4 +19,5 @@ app.register_blueprint(cliente_blueprint)
 app.register_blueprint(funcionario_blueprint)
 app.register_blueprint(lanchonete_blueprint)
 app.register_blueprint(item_blueprint)
+app.register_blueprint(receita_blueprint)
 app.run("0.0.0.0", port=8000, debug=False)
