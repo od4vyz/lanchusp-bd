ec1c5702c171f6804011b0f23bc9e19c7a3a0b6c
André Portela <andreplino@gmail.com>
2025-11-24 18:45:10 -0300

    CRUD Item Base

--- backend/rotas/item.py
+++ backend/rotas/item.py
@@ -9,33 +9,39 @@ item_blueprint = Blueprint("item", __name__)
 def get_item():    
     # Parâmetros - Filtra por nome, lanchonete_end ou categoria
     nome = request.args.get("nome", "")
-    lanchonete_end = request.args.get("lanchonete_end", "")
-    categoria = request.args.get("categoria", "")
-    return jsonify(ItemDatabase().get_item(nome, lanchonete_end, categoria)), 200
+    return jsonify(ItemDatabase().get_item(nome)), 200
 
 # Insere um Item novo
 @item_blueprint.route("/item", methods=["POST"])
 def insere_item():
     json = request.get_json()
     nome = json.get("nome")
-    lanchonete_end = json.get("lanchonete_end")
-    categoria = json.get("categoria")
-    registro = ItemDatabase().regristra_item(nome, lanchonete_end, categoria)
+    registro = ItemDatabase().regristra_item(nome)
 
     if not registro:
         return jsonify("Não foi possível registrar o item"), 400
 
     return jsonify("Item registrado com sucesso!"), 200
 
-# Remove um Cliente
+# Remove um Item
 @item_blueprint.route("/item", methods=["DELETE"])
 def deleta_item():
     nome = request.args.get("nome", "")
-    lanchonete_end = request.args.get("lanchonete_end", "")
-    categoria = request.args.get("categoria", "")
-    delete = ItemDatabase().remove_item(nome, lanchonete_end, categoria)
+    delete = ItemDatabase().remove_item(nome)
 
     if not delete:
         return jsonify("Não foi possível solicitar a remoção do Item"), 400
 
     return jsonify("Remoção do Item solicitada com sucesso!"), 200
+
+# Atualiza um Item
+@item_blueprint.route("/item", methods=["PUT"])
+def atualiza_item():
+    nome = request.args.get("nome", "")
+    nome_novo = request.args.get("nome_novo", "")
+    update = ItemDatabase().atualiza_item(nome, nome_novo)
+
+    if not update:
+        return jsonify("Não foi possível solicitar a atualização do item"), 400
+
+    return jsonify("Atualização do item solicitada com sucesso!"), 200
